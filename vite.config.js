944c154597d3976a8367765e3189dfc8978b44a9
haseebkodx <haseeb@kodxsystem.com>
2024-04-27 01:40:33 +0500

    build on github

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { createVuePlugin } from "vite-plugin-vue2";
 import { VuetifyResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
+import { createVuePlugin } from "vite-plugin-vue2";
 
 export default {
     server: {
