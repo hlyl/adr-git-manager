1a6cbc8c0df9f1dbc9822ff7d6d6598e0a696f48
haseebkodx <haseeb@kodxsystem.com>
2024-05-15 19:53:52 +0500

    ht

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -1,43 +1,42 @@
-import { LogLevel, PublicClientApplication,InteractionRequiredAuthError }  from '@azure/msal-browser';
+import { InteractionRequiredAuthError, LogLevel, PublicClientApplication } from '@azure/msal-browser';
 
 // Config object to be passed to Msal on creation
 const msalConfig = {
-  auth: {
-    clientId: '50201ca6-d618-4715-9920-03909523f4ea',
-    authority: 'https://login.microsoftonline.com/56841a8d-d6d0-40c5-a7e6-26fa3cd19c0b',
-    redirectUri: '/', // Must be registered as a SPA redirectURI on your app registration
-    postLogoutRedirectUri: '/' // Must be registered as a SPA redirectURI on your app registration
-  },
-  cache: {
-    cacheLocation: 'localStorage'
-  },
-  system: {
-      loggerOptions: {
-          loggerCallback: (level, message, containsPii) => {
-              if (containsPii) {
-                  return;
-              }
-              switch (level) {
-                  case LogLevel.Error:
-                      console.error(message);
-                      return;
-                  case LogLevel.Info:
-                      console.info(message);
-                      return;
-                  case LogLevel.Verbose:
-                      console.debug(message);
-                      return;
-                  case LogLevel.Warning:
-                      console.warn(message);
-                      return;
-                  default:
-                      return;
-              }
-          },
-          logLevel: LogLevel.Verbose
-      }
-  },
-  
+    auth: {
+        clientId: "3a9a7e6d-87e0-4484-87ec-dc326cade1cc",
+        authority: "https://login.microsoftonline.com/56841a8d-d6d0-40c5-a7e6-26fa3cd19c0b",
+        redirectUri: "/", // Must be registered as a SPA redirectURI on your app registration
+        postLogoutRedirectUri: "/" // Must be registered as a SPA redirectURI on your app registration
+    },
+    cache: {
+        cacheLocation: "localStorage"
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (level, message, containsPii) => {
+                if (containsPii) {
+                    return;
+                }
+                switch (level) {
+                    case LogLevel.Error:
+                        console.error(message);
+                        return;
+                    case LogLevel.Info:
+                        console.info(message);
+                        return;
+                    case LogLevel.Verbose:
+                        console.debug(message);
+                        return;
+                    case LogLevel.Warning:
+                        console.warn(message);
+                        return;
+                    default:
+                        return;
+                }
+            },
+            logLevel: LogLevel.Verbose
+        }
+    }
 };
 
 const msalInstance = new PublicClientApplication(msalConfig);
